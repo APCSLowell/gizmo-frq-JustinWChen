d6ff0d6fab620673317897b8090161a869e33d10
JustinWChen <143028009+JustinWChen@users.noreply.github.com>
2024-04-14 21:23:35 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -29,7 +29,7 @@ public class OnlinePurchaseManager
     if (purchases.size()<2)
       return false;
     for (int i = 1; i < purchases.size()-1;i++){
-    if (purchases.get(i)==purchases.get(i-1))
+    if (purchases.get(i).equals(purchases.get(i-1)))
       return true;
     }
     return false;
